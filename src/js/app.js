8118ff16b59fa8d0b79d43fb143fb20aee7ca2d2
Maciej Moroz <maciej.moroz89@gmail.com>
2019-06-11 23:53:15 +0200

    css fixes add products section

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,7 @@
-var BODY = this.document.querySelector("body"),
+const BODY = this.document.querySelector("body"),
   btn_menu = this.document.querySelector("[data-menu-btn]");
 
 // hamburger menu
 btn_menu.onclick = function() {
   BODY.classList.toggle("open-menu");
 };
-
-var coll = document.getElementsByClassName("colapse");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
