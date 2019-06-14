5322f4edfe7edc2312a0f22b7fcea97eb5e8be9d
Maciej Moroz <maciej.moroz89@gmail.com>
2019-06-14 22:11:18 +0200

    css fixes footer fixed nav

--- web/js/app.js
+++ web/js/app.js
@@ -2,6 +2,23 @@ const BODY = this.document.querySelector("body"),
   btn_menu = this.document.querySelector("[data-menu-btn]");
 
 // hamburger menu
-btn_menu.onclick = function() {
+btn_menu.onclick = () => {
   BODY.classList.toggle("open-menu");
+};
+
+//fixed menu
+let nav = document.getElementById("nav");
+
+window.onscroll = () => {
+  if (window.innerWidth > 1024) {
+    window.pageYOffset > 100
+      ? (nav.style.background = "rgba(112, 112, 112, 0.9)")(
+          (nav.style.height = "120px")
+        )
+      : (nav.style.background = "transparent");
+  } else {
+    (nav.style.background = "rgba(112, 112, 112, 0.9)")(
+      (nav.style.height = "120px")
+    );
+  }
 };
